f4d4934a2a828b603dd4b6a79cfdbfa25a563996
isyang1223 <isyang1223@gmail.com>
2017-09-26 20:51:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,14 @@ function whileLoop(n){
   }
   return 'done'
 }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+do {
+  array.slice(1)
+} while (array.length > 0 && maybeTrue()) 
+}
+return array
+}
